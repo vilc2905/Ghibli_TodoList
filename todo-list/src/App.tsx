80294c20cc2387038afd5d0ada24c11d699ad8cf
Caroleannv <43645688+Caroleannv@users.noreply.github.com>
2022-07-01 11:36:25 -0400

    Create simple glassmorphism container

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, TextField } from '@mui/material';
+import { styled, createTheme } from "@mui/system"
+import background from './test-bg.jpg';
+import { Container, CssBaseline, Grid, Typography } from '@mui/material';
+import glassCard from './glassCard.module.css';
+
+const Background = styled('div')({
+  backgroundImage: `url(${background})`,
+  height: "100vh",
+  width: "100%",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundAttachment: "fixed",
+});
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Background>
+      <CssBaseline />
+      <Container maxWidth="sm" className={glassCard.card}
+        sx={{ height: 700, width: 300}}>
+
+      </Container>
+    </Background>
   );
 }
 
 export default App;
+
